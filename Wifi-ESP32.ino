79b532b4f2b9ba0a36bccde41e55503c7e592b04
Julian Ruiz <bendertrator@gmail.com>
2022-05-18 23:45:57 -0500

    include wlan and paswwd data

--- Wifi-ESP32.ino
+++ Wifi-ESP32.ino
@@ -1,6 +1,8 @@
-#include <WiFi.h>
-
-
+#include <WiFi.h> // Include wifi library
+--------------------------------------------
+#define WLAN  ""  // Include WLAN name
+#define PASSWD "" // Include PASSWD of WLAN
+--------------------------------------------
 void setup() {
   // put your setup code here, to run once:
 
